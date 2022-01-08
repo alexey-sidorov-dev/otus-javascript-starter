a1a4c2989f77753bfbd6e02aac87c99b92ed8f04
Alexey Sidorov <alexey.sidorov.dev@mail.ru>
2022-01-08 22:33:46 +0300

    add homework-10

---
+++ src/homework-10/isEmailAddress.test.js
@@ -0,0 +1,23 @@
+import { isEmailAddress } from "./isEmailAddress";
+
+describe("check function isEmailAddress", () => {
+  it("should return true for mail@mail.ru", () => {
+    jest.spyOn(window, "prompt").mockReturnValue("mail@mail.ru");
+    expect(isEmailAddress()).toBeTruthy();
+  });
+
+  it("should return true for mail.mail@mail.ru", () => {
+    jest.spyOn(window, "prompt").mockReturnValue("mail.mail@mail.ru");
+    expect(isEmailAddress()).toBeTruthy();
+  });
+
+  it("should return false for asd", () => {
+    jest.spyOn(window, "prompt").mockReturnValue("asd");
+    expect(isEmailAddress()).not.toBeTruthy();
+  });
+
+  it("should return false for null", () => {
+    jest.spyOn(window, "prompt").mockReturnValue(null);
+    expect(isEmailAddress()).not.toBeTruthy();
+  });
+});
---
+++ src/homework-10/isPhoneNumber.test.js
@@ -0,0 +1,23 @@
+import { isPhoneNUmber } from "./isPhoneNumber";
+
+describe("check function isPhoneNUmber", () => {
+  it("should return true for +74991234567", () => {
+    jest.spyOn(window, "prompt").mockReturnValue("+74991234567");
+    expect(isPhoneNUmber()).toBeTruthy();
+  });
+
+  it("should return true for 8 495 765 43 21", () => {
+    jest.spyOn(window, "prompt").mockReturnValue("+7 495 765 43 21");
+    expect(isPhoneNUmber()).toBeTruthy();
+  });
+
+  it("should return false for asd", () => {
+    jest.spyOn(window, "prompt").mockReturnValue("asd");
+    expect(isPhoneNUmber()).not.toBeTruthy();
+  });
+
+  it("should return false for null", () => {
+    jest.spyOn(window, "prompt").mockReturnValue(null);
+    expect(isPhoneNUmber()).not.toBeTruthy();
+  });
+});
